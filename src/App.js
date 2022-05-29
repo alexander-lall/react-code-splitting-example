a34f509b4377e05bb23a31b3fc43e60f1848881d
Alexander Lall <alexander.heddier@gmx.de>
2022-05-29 14:46:06 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 import Page1 from './Components/Page1';
 
-const Page2 = React.lazy(() => import('./Components/Page2'));
-const Page3 = React.lazy(() => import('./Components/Page3'));
+const Page2Lazy = React.lazy(() => import('./Components/Page2'));
+const Page3Lazy = React.lazy(() => import('./Components/Page3'));
 
 class App extends Component {
   constructor() {
@@ -23,13 +23,13 @@ class App extends Component {
     } else if (this.state.route === 'page2') {
       return (
         <Suspense fallback={<div>Loading...</div>}>
-          <Page2 onRouteChange={this.onRouteChange} />
+          <Page2Lazy onRouteChange={this.onRouteChange} />
         </Suspense>
       ); 
     } else {
       return (
         <Suspense fallback={<div>Loading...</div>}>
-          <Page3 onRouteChange={this.onRouteChange} />
+          <Page3Lazy onRouteChange={this.onRouteChange} />
         </Suspense>
       );
     }
